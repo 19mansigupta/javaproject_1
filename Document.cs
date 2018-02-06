616a8c8a40f9fbe71a4ff6ea86663f523c93115e
rishikagupta2437 <rishikagupta2013@gmail,com>
2018-02-06 13:47:41 +0530

    project submit

---
+++ Document.cs
@@ -0,0 +1,192 @@
+file name:-DOCUMENT vir1.docx,path:-G:\new files\docc\computer graphics\DOCUMENT vir1.docx
+file name:-ER DIAGRAM.docx,path:-G:\new files\docc\computer graphics\ER DIAGRAM.docx
+file name:-OTHERS.docx,path:-G:\new files\docc\computer graphics\OTHERS.docx
+file name:-UMML.docx,path:-G:\new files\docc\computer graphics\UMML.docx
+file name:-VIR2.docx,path:-G:\new files\docc\computer graphics\VIR2.docx
+file name:-2008-paper-ii_0.pdf,path:-G:\new files\docc\isro\2008-paper-ii_0.pdf
+file name:-2008-paper-i_0.pdf,path:-G:\new files\docc\isro\2008-paper-i_0.pdf
+file name:-2010-paper-ii_0.pdf,path:-G:\new files\docc\isro\2010-paper-ii_0.pdf
+file name:-2010-paper-i_0.pdf,path:-G:\new files\docc\isro\2010-paper-i_0.pdf
+file name:-2012_0.pdf,path:-G:\new files\docc\isro\2012_0.pdf
+file name:-2014-a_1.pdf,path:-G:\new files\docc\isro\2014-a_1.pdf
+file name:-2015-a.pdf,path:-G:\new files\docc\isro\2015-a.pdf
+file name:-2D transformation 07-Mar-2017 20-46-19.pdf,path:-G:\new files\docc\mca 4th sem\2D transformation 07-Mar-2017 20-46-19.pdf
+file name:-ADA assignments & solutions 17-Mar-2017 17-10-34.pdf,path:-G:\new files\docc\mca 4th sem\ADA assignments & solutions 17-Mar-2017 17-10-34.pdf
+file name:-Bezier method 27-Mar-2017 20-54-55.pdf,path:-G:\new files\docc\mca 4th sem\Bezier method 27-Mar-2017 20-54-55.pdf
+file name:-dt.pdf.pdf,path:-G:\new files\docc\mca 4th sem\dt.pdf.pdf
+file name:-dtad.pdf.pdf,path:-G:\new files\docc\mca 4th sem\dtad.pdf.pdf
+file name:-JAVA 4th Unit 05-Apr-2017 17-53-54.pdf,path:-G:\new files\docc\mca 4th sem\JAVA 4th Unit 05-Apr-2017 17-53-54.pdf
+file name:-MC 1st Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 1st Unit.docx
+file name:-MC 27-Mar-2017 21-03-56.pdf,path:-G:\new files\docc\mca 4th sem\MC 27-Mar-2017 21-03-56.pdf
+file name:-MC 2nd Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 2nd Unit.docx
+file name:-MC 3rd Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 3rd Unit.docx
+file name:-MC 4th Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 4th Unit.docx
+file name:-MCA 5th Unit.docx,path:-G:\new files\docc\mca 4th sem\MCA 5th Unit.docx
+file name:-MCA IV th SEM Papers 2013 13-Feb-2017 19-34-31.pdf,path:-G:\new files\docc\mca 4th sem\MCA IV th SEM Papers 2013 13-Feb-2017 19-34-31.pdf
+file name:-MCA IVth SEM Papers 2012 13-Feb-2017 19-39-12.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2012 13-Feb-2017 19-39-12.pdf
+file name:-MCA IVth SEM Papers 2014 13-Feb-2017 19-29-14.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2014 13-Feb-2017 19-29-14.pdf
+file name:-MCA IVth SEM Papers 2015 13-Feb-2017 19-25-48.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2015 13-Feb-2017 19-25-48.pdf
+file name:-MCA IVth SEM Papers 2016 13-Feb-2017 19-19-04.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2016 13-Feb-2017 19-19-04.pdf
+file name:-Unit 3rd java 07-Apr-2017 22-04-11.pdf,path:-G:\new files\docc\mca 4th sem\Unit 3rd java 07-Apr-2017 22-04-11.pdf
+file name:-ANIE FRONT.docx,path:-G:\new files\docc\MOBILE WORLD\ANIE FRONT.docx
+file name:-DIVYA FRONT - Copy.docx,path:-G:\new files\docc\MOBILE WORLD\DIVYA FRONT - Copy.docx
+file name:-mobile world - DIVYA.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world - DIVYA.docx
+file name:-mobile world RISHIKA.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world RISHIKA.docx
+file name:-mobile world.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world.docx
+file name:-RISHI FRONT - Copy.docx,path:-G:\new files\docc\MOBILE WORLD\RISHI FRONT - Copy.docx
+file name:-IndexerVolumeGuid,path:-G:\new files\docc\System Volume Information\IndexerVolumeGuid
+file name:-WPSettings.dat,path:-G:\new files\docc\System Volume Information\WPSettings.dat
+file name:-AI 4th Unit.docx,path:-G:\new files\docc\AI 4th Unit.docx
+file name:-AI 5th Unit.docx,path:-G:\new files\docc\AI 5th Unit.docx
+file name:-CERTIFICATE.docx,path:-G:\new files\docc\CERTIFICATE.docx
+file name:-Distributed_System.pdf,path:-G:\new files\docc\Distributed_System.pdf
+file name:-New Document(3).pdf,path:-G:\new files\docc\New Document(3).pdf
+file name:-New Microsoft Word Document.docx,path:-G:\new files\docc\New Microsoft Word Document.docx
+file name:-Synopsis.docx,path:-G:\new files\docc\Synopsis.docx
+file name:-TCS-Latest-Placement-Paper-Questions.docx,path:-G:\new files\docc\TCS-Latest-Placement-Paper-Questions.docx
+file name:-VIR.docx,path:-G:\new files\docc\VIR.docx
+file name:-VIRR.docx,path:-G:\new files\docc\VIRR.docx
+file name:-DOCUMENT vir1.docx,path:-G:\new files\docc\computer graphics\DOCUMENT vir1.docx
+file name:-ER DIAGRAM.docx,path:-G:\new files\docc\computer graphics\ER DIAGRAM.docx
+file name:-OTHERS.docx,path:-G:\new files\docc\computer graphics\OTHERS.docx
+file name:-UMML.docx,path:-G:\new files\docc\computer graphics\UMML.docx
+file name:-VIR2.docx,path:-G:\new files\docc\computer graphics\VIR2.docx
+file name:-2008-paper-ii_0.pdf,path:-G:\new files\docc\isro\2008-paper-ii_0.pdf
+file name:-2008-paper-i_0.pdf,path:-G:\new files\docc\isro\2008-paper-i_0.pdf
+file name:-2010-paper-ii_0.pdf,path:-G:\new files\docc\isro\2010-paper-ii_0.pdf
+file name:-2010-paper-i_0.pdf,path:-G:\new files\docc\isro\2010-paper-i_0.pdf
+file name:-2012_0.pdf,path:-G:\new files\docc\isro\2012_0.pdf
+file name:-2014-a_1.pdf,path:-G:\new files\docc\isro\2014-a_1.pdf
+file name:-2015-a.pdf,path:-G:\new files\docc\isro\2015-a.pdf
+file name:-2D transformation 07-Mar-2017 20-46-19.pdf,path:-G:\new files\docc\mca 4th sem\2D transformation 07-Mar-2017 20-46-19.pdf
+file name:-ADA assignments & solutions 17-Mar-2017 17-10-34.pdf,path:-G:\new files\docc\mca 4th sem\ADA assignments & solutions 17-Mar-2017 17-10-34.pdf
+file name:-Bezier method 27-Mar-2017 20-54-55.pdf,path:-G:\new files\docc\mca 4th sem\Bezier method 27-Mar-2017 20-54-55.pdf
+file name:-dt.pdf.pdf,path:-G:\new files\docc\mca 4th sem\dt.pdf.pdf
+file name:-dtad.pdf.pdf,path:-G:\new files\docc\mca 4th sem\dtad.pdf.pdf
+file name:-JAVA 4th Unit 05-Apr-2017 17-53-54.pdf,path:-G:\new files\docc\mca 4th sem\JAVA 4th Unit 05-Apr-2017 17-53-54.pdf
+file name:-MC 1st Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 1st Unit.docx
+file name:-MC 27-Mar-2017 21-03-56.pdf,path:-G:\new files\docc\mca 4th sem\MC 27-Mar-2017 21-03-56.pdf
+file name:-MC 2nd Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 2nd Unit.docx
+file name:-MC 3rd Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 3rd Unit.docx
+file name:-MC 4th Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 4th Unit.docx
+file name:-MCA 5th Unit.docx,path:-G:\new files\docc\mca 4th sem\MCA 5th Unit.docx
+file name:-MCA IV th SEM Papers 2013 13-Feb-2017 19-34-31.pdf,path:-G:\new files\docc\mca 4th sem\MCA IV th SEM Papers 2013 13-Feb-2017 19-34-31.pdf
+file name:-MCA IVth SEM Papers 2012 13-Feb-2017 19-39-12.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2012 13-Feb-2017 19-39-12.pdf
+file name:-MCA IVth SEM Papers 2014 13-Feb-2017 19-29-14.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2014 13-Feb-2017 19-29-14.pdf
+file name:-MCA IVth SEM Papers 2015 13-Feb-2017 19-25-48.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2015 13-Feb-2017 19-25-48.pdf
+file name:-MCA IVth SEM Papers 2016 13-Feb-2017 19-19-04.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2016 13-Feb-2017 19-19-04.pdf
+file name:-Unit 3rd java 07-Apr-2017 22-04-11.pdf,path:-G:\new files\docc\mca 4th sem\Unit 3rd java 07-Apr-2017 22-04-11.pdf
+file name:-ANIE FRONT.docx,path:-G:\new files\docc\MOBILE WORLD\ANIE FRONT.docx
+file name:-DIVYA FRONT - Copy.docx,path:-G:\new files\docc\MOBILE WORLD\DIVYA FRONT - Copy.docx
+file name:-mobile world - DIVYA.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world - DIVYA.docx
+file name:-mobile world RISHIKA.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world RISHIKA.docx
+file name:-mobile world.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world.docx
+file name:-RISHI FRONT - Copy.docx,path:-G:\new files\docc\MOBILE WORLD\RISHI FRONT - Copy.docx
+file name:-IndexerVolumeGuid,path:-G:\new files\docc\System Volume Information\IndexerVolumeGuid
+file name:-WPSettings.dat,path:-G:\new files\docc\System Volume Information\WPSettings.dat
+file name:-AI 4th Unit.docx,path:-G:\new files\docc\AI 4th Unit.docx
+file name:-AI 5th Unit.docx,path:-G:\new files\docc\AI 5th Unit.docx
+file name:-CERTIFICATE.docx,path:-G:\new files\docc\CERTIFICATE.docx
+file name:-Distributed_System.pdf,path:-G:\new files\docc\Distributed_System.pdf
+file name:-New Document(3).pdf,path:-G:\new files\docc\New Document(3).pdf
+file name:-New Microsoft Word Document.docx,path:-G:\new files\docc\New Microsoft Word Document.docx
+file name:-Synopsis.docx,path:-G:\new files\docc\Synopsis.docx
+file name:-TCS-Latest-Placement-Paper-Questions.docx,path:-G:\new files\docc\TCS-Latest-Placement-Paper-Questions.docx
+file name:-VIR.docx,path:-G:\new files\docc\VIR.docx
+file name:-VIRR.docx,path:-G:\new files\docc\VIRR.docx
+file name:-DOCUMENT vir1.docx,path:-G:\new files\docc\computer graphics\DOCUMENT vir1.docx
+file name:-ER DIAGRAM.docx,path:-G:\new files\docc\computer graphics\ER DIAGRAM.docx
+file name:-OTHERS.docx,path:-G:\new files\docc\computer graphics\OTHERS.docx
+file name:-UMML.docx,path:-G:\new files\docc\computer graphics\UMML.docx
+file name:-VIR2.docx,path:-G:\new files\docc\computer graphics\VIR2.docx
+file name:-2008-paper-ii_0.pdf,path:-G:\new files\docc\isro\2008-paper-ii_0.pdf
+file name:-2008-paper-i_0.pdf,path:-G:\new files\docc\isro\2008-paper-i_0.pdf
+file name:-2010-paper-ii_0.pdf,path:-G:\new files\docc\isro\2010-paper-ii_0.pdf
+file name:-2010-paper-i_0.pdf,path:-G:\new files\docc\isro\2010-paper-i_0.pdf
+file name:-2012_0.pdf,path:-G:\new files\docc\isro\2012_0.pdf
+file name:-2014-a_1.pdf,path:-G:\new files\docc\isro\2014-a_1.pdf
+file name:-2015-a.pdf,path:-G:\new files\docc\isro\2015-a.pdf
+file name:-2D transformation 07-Mar-2017 20-46-19.pdf,path:-G:\new files\docc\mca 4th sem\2D transformation 07-Mar-2017 20-46-19.pdf
+file name:-ADA assignments & solutions 17-Mar-2017 17-10-34.pdf,path:-G:\new files\docc\mca 4th sem\ADA assignments & solutions 17-Mar-2017 17-10-34.pdf
+file name:-Bezier method 27-Mar-2017 20-54-55.pdf,path:-G:\new files\docc\mca 4th sem\Bezier method 27-Mar-2017 20-54-55.pdf
+file name:-dt.pdf.pdf,path:-G:\new files\docc\mca 4th sem\dt.pdf.pdf
+file name:-dtad.pdf.pdf,path:-G:\new files\docc\mca 4th sem\dtad.pdf.pdf
+file name:-JAVA 4th Unit 05-Apr-2017 17-53-54.pdf,path:-G:\new files\docc\mca 4th sem\JAVA 4th Unit 05-Apr-2017 17-53-54.pdf
+file name:-MC 1st Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 1st Unit.docx
+file name:-MC 27-Mar-2017 21-03-56.pdf,path:-G:\new files\docc\mca 4th sem\MC 27-Mar-2017 21-03-56.pdf
+file name:-MC 2nd Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 2nd Unit.docx
+file name:-MC 3rd Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 3rd Unit.docx
+file name:-MC 4th Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 4th Unit.docx
+file name:-MCA 5th Unit.docx,path:-G:\new files\docc\mca 4th sem\MCA 5th Unit.docx
+file name:-MCA IV th SEM Papers 2013 13-Feb-2017 19-34-31.pdf,path:-G:\new files\docc\mca 4th sem\MCA IV th SEM Papers 2013 13-Feb-2017 19-34-31.pdf
+file name:-MCA IVth SEM Papers 2012 13-Feb-2017 19-39-12.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2012 13-Feb-2017 19-39-12.pdf
+file name:-MCA IVth SEM Papers 2014 13-Feb-2017 19-29-14.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2014 13-Feb-2017 19-29-14.pdf
+file name:-MCA IVth SEM Papers 2015 13-Feb-2017 19-25-48.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2015 13-Feb-2017 19-25-48.pdf
+file name:-MCA IVth SEM Papers 2016 13-Feb-2017 19-19-04.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2016 13-Feb-2017 19-19-04.pdf
+file name:-Unit 3rd java 07-Apr-2017 22-04-11.pdf,path:-G:\new files\docc\mca 4th sem\Unit 3rd java 07-Apr-2017 22-04-11.pdf
+file name:-ANIE FRONT.docx,path:-G:\new files\docc\MOBILE WORLD\ANIE FRONT.docx
+file name:-DIVYA FRONT - Copy.docx,path:-G:\new files\docc\MOBILE WORLD\DIVYA FRONT - Copy.docx
+file name:-mobile world - DIVYA.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world - DIVYA.docx
+file name:-mobile world RISHIKA.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world RISHIKA.docx
+file name:-mobile world.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world.docx
+file name:-RISHI FRONT - Copy.docx,path:-G:\new files\docc\MOBILE WORLD\RISHI FRONT - Copy.docx
+file name:-IndexerVolumeGuid,path:-G:\new files\docc\System Volume Information\IndexerVolumeGuid
+file name:-WPSettings.dat,path:-G:\new files\docc\System Volume Information\WPSettings.dat
+file name:-AI 4th Unit.docx,path:-G:\new files\docc\AI 4th Unit.docx
+file name:-AI 5th Unit.docx,path:-G:\new files\docc\AI 5th Unit.docx
+file name:-CERTIFICATE.docx,path:-G:\new files\docc\CERTIFICATE.docx
+file name:-Distributed_System.pdf,path:-G:\new files\docc\Distributed_System.pdf
+file name:-New Document(3).pdf,path:-G:\new files\docc\New Document(3).pdf
+file name:-New Microsoft Word Document.docx,path:-G:\new files\docc\New Microsoft Word Document.docx
+file name:-Synopsis.docx,path:-G:\new files\docc\Synopsis.docx
+file name:-TCS-Latest-Placement-Paper-Questions.docx,path:-G:\new files\docc\TCS-Latest-Placement-Paper-Questions.docx
+file name:-VIR.docx,path:-G:\new files\docc\VIR.docx
+file name:-VIRR.docx,path:-G:\new files\docc\VIRR.docx
+file name:-DOCUMENT vir1.docx,path:-G:\new files\docc\computer graphics\DOCUMENT vir1.docx
+file name:-ER DIAGRAM.docx,path:-G:\new files\docc\computer graphics\ER DIAGRAM.docx
+file name:-OTHERS.docx,path:-G:\new files\docc\computer graphics\OTHERS.docx
+file name:-UMML.docx,path:-G:\new files\docc\computer graphics\UMML.docx
+file name:-VIR2.docx,path:-G:\new files\docc\computer graphics\VIR2.docx
+file name:-2008-paper-ii_0.pdf,path:-G:\new files\docc\isro\2008-paper-ii_0.pdf
+file name:-2008-paper-i_0.pdf,path:-G:\new files\docc\isro\2008-paper-i_0.pdf
+file name:-2010-paper-ii_0.pdf,path:-G:\new files\docc\isro\2010-paper-ii_0.pdf
+file name:-2010-paper-i_0.pdf,path:-G:\new files\docc\isro\2010-paper-i_0.pdf
+file name:-2012_0.pdf,path:-G:\new files\docc\isro\2012_0.pdf
+file name:-2014-a_1.pdf,path:-G:\new files\docc\isro\2014-a_1.pdf
+file name:-2015-a.pdf,path:-G:\new files\docc\isro\2015-a.pdf
+file name:-2D transformation 07-Mar-2017 20-46-19.pdf,path:-G:\new files\docc\mca 4th sem\2D transformation 07-Mar-2017 20-46-19.pdf
+file name:-ADA assignments & solutions 17-Mar-2017 17-10-34.pdf,path:-G:\new files\docc\mca 4th sem\ADA assignments & solutions 17-Mar-2017 17-10-34.pdf
+file name:-Bezier method 27-Mar-2017 20-54-55.pdf,path:-G:\new files\docc\mca 4th sem\Bezier method 27-Mar-2017 20-54-55.pdf
+file name:-dt.pdf.pdf,path:-G:\new files\docc\mca 4th sem\dt.pdf.pdf
+file name:-dtad.pdf.pdf,path:-G:\new files\docc\mca 4th sem\dtad.pdf.pdf
+file name:-JAVA 4th Unit 05-Apr-2017 17-53-54.pdf,path:-G:\new files\docc\mca 4th sem\JAVA 4th Unit 05-Apr-2017 17-53-54.pdf
+file name:-MC 1st Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 1st Unit.docx
+file name:-MC 27-Mar-2017 21-03-56.pdf,path:-G:\new files\docc\mca 4th sem\MC 27-Mar-2017 21-03-56.pdf
+file name:-MC 2nd Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 2nd Unit.docx
+file name:-MC 3rd Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 3rd Unit.docx
+file name:-MC 4th Unit.docx,path:-G:\new files\docc\mca 4th sem\MC 4th Unit.docx
+file name:-MCA 5th Unit.docx,path:-G:\new files\docc\mca 4th sem\MCA 5th Unit.docx
+file name:-MCA IV th SEM Papers 2013 13-Feb-2017 19-34-31.pdf,path:-G:\new files\docc\mca 4th sem\MCA IV th SEM Papers 2013 13-Feb-2017 19-34-31.pdf
+file name:-MCA IVth SEM Papers 2012 13-Feb-2017 19-39-12.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2012 13-Feb-2017 19-39-12.pdf
+file name:-MCA IVth SEM Papers 2014 13-Feb-2017 19-29-14.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2014 13-Feb-2017 19-29-14.pdf
+file name:-MCA IVth SEM Papers 2015 13-Feb-2017 19-25-48.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2015 13-Feb-2017 19-25-48.pdf
+file name:-MCA IVth SEM Papers 2016 13-Feb-2017 19-19-04.pdf,path:-G:\new files\docc\mca 4th sem\MCA IVth SEM Papers 2016 13-Feb-2017 19-19-04.pdf
+file name:-Unit 3rd java 07-Apr-2017 22-04-11.pdf,path:-G:\new files\docc\mca 4th sem\Unit 3rd java 07-Apr-2017 22-04-11.pdf
+file name:-ANIE FRONT.docx,path:-G:\new files\docc\MOBILE WORLD\ANIE FRONT.docx
+file name:-DIVYA FRONT - Copy.docx,path:-G:\new files\docc\MOBILE WORLD\DIVYA FRONT - Copy.docx
+file name:-mobile world - DIVYA.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world - DIVYA.docx
+file name:-mobile world RISHIKA.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world RISHIKA.docx
+file name:-mobile world.docx,path:-G:\new files\docc\MOBILE WORLD\mobile world.docx
+file name:-RISHI FRONT - Copy.docx,path:-G:\new files\docc\MOBILE WORLD\RISHI FRONT - Copy.docx
+file name:-IndexerVolumeGuid,path:-G:\new files\docc\System Volume Information\IndexerVolumeGuid
+file name:-WPSettings.dat,path:-G:\new files\docc\System Volume Information\WPSettings.dat
+file name:-AI 4th Unit.docx,path:-G:\new files\docc\AI 4th Unit.docx
+file name:-AI 5th Unit.docx,path:-G:\new files\docc\AI 5th Unit.docx
+file name:-CERTIFICATE.docx,path:-G:\new files\docc\CERTIFICATE.docx
+file name:-Distributed_System.pdf,path:-G:\new files\docc\Distributed_System.pdf
+file name:-New Document(3).pdf,path:-G:\new files\docc\New Document(3).pdf
+file name:-New Microsoft Word Document.docx,path:-G:\new files\docc\New Microsoft Word Document.docx
+file name:-Synopsis.docx,path:-G:\new files\docc\Synopsis.docx
+file name:-TCS-Latest-Placement-Paper-Questions.docx,path:-G:\new files\docc\TCS-Latest-Placement-Paper-Questions.docx
+file name:-VIR.docx,path:-G:\new files\docc\VIR.docx
+file name:-VIRR.docx,path:-G:\new files\docc\VIRR.docx
